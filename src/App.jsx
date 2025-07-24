7854594ec3744f774fcd398143da256275a88872
Daniru72 <Daniru@dev.athenaholdingsllc.com>
2025-07-24 23:58:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='border border-gray-300 p-4'>
+      <h1 className='text-xl font-bold te'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
